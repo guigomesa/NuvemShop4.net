8933a6fb33dd25fd2800fea33ee787233a277289
Guilherme Almeida <guigomesa@outlook.com>
2017-09-20 15:45:06 -0300

    add store nuvem extension

---
+++ NuvemShopApi/ExtensionsNuvem/StoreNuvemExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuvemShopApi.ExtensionsNuvem
+{
+    public static class StoreNuvemExtension
+    {
+        public const string BaseResourceCoupom = "/store";
+
+        public static T GetStore<T>(this ClientNuvemShop apiClient)
+        {
+            return apiClient.GetData<T>($"{BaseResourceCoupom}");
+        }
+    }
+}
